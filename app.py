476165ef15157ed0550f97c1705bbcc746401dcd
Qreede <qreedecomp@gmail.com>
2025-11-12 15:32:34 +0100

    Fix requirements and start command for Render

--- app.py
+++ app.py
@@ -160,9 +160,11 @@ def delete_qr(qr_file):
 
 # ---------------- RUN APP ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    debug = os.environ.get("DEBUG", "False") == "True"
+    app.run(host="0.0.0.0", port=port, debug=debug)
+
+
 
 
 
